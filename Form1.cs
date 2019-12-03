5e847f0a6e842a70ae757532105203ca205fe84b
tianmingz@hotmail.com <ztm@pass@word1>
2019-12-04 08:00:00 +0900

    temp

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Translation.V2;
+
+namespace AutoTranslateFields
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string credential_path = @"C:\dev\My First Project-1bae3efe78ae.json";
+
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential_path);
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+            var response = client.TranslateText("与信管理", "en", "ja");
+            Console.WriteLine(response.TranslatedText);
+        }
+    }
+}
